e9d2e71af05c7e64dd7c5d921bf75956fff0434a
Daniel <daniel_katz@hotmail.ca>
2018-05-15 11:57:47 -0700

    Record log merged

--- Keep Pace iOS/UserLogViewController.swift
+++ Keep Pace iOS/UserLogViewController.swift
@@ -8,15 +8,45 @@
 
 import UIKit
 
-class UserLogViewController: UIViewController {
+class UserLogViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 7
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "recordCell", for: indexPath) as! ReusableRecordCell
+        let recordView = cell.recordCell as! RecordViewTemplate
+        let dbHelper = DatabaseHelper()
+        let raceModel = dbHelper.getRaceModel(idToLookFor: indexPath.row)
+        recordView.nameLabel.text = raceModel?.mName
+//        recordView.paceLabel.text = String(raceModel?.mAveragePace)
+//
+//        let recordModel = raceModel?.getBestRecord()
+//        recordView.bestLabel.text = String(recordModel?.mTime)
+        
+        return cell
+    }
+    @IBOutlet weak var tableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 
+    @IBOutlet weak var headerView: RecordViewTemplate!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.delegate = self
+        tableView.dataSource = self
         //Navigation bar title text
         self.navigationController?.navigationBar.titleTextAttributes = [ NSAttributedStringKey.font: UIFont(name: "Racing Sans One", size: 20)!, NSAttributedStringKey.foregroundColor : UIColor.white]
         navigationItem.backBarButtonItem = UIBarButtonItem(title: "Back", style: .plain, target: nil, action: nil)
+        
+        headerView.nameLabel.text = "Name"
+        headerView.paceLabel.text = "Avg. Pace"
+        headerView.bestLabel.text = "Best Time"
+        
+        tableView.tableFooterView = UIView()
     }
 
     override func didReceiveMemoryWarning() {
